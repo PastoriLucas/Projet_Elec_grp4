3ceef15b1c636f88d1daa7548783c79eaf552907
Matthieu Luyckx <he201687@students.ephec.be>
2020-05-09 14:50:56 +0200

    merge

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/19/005180e73291001a14d1de7ffea2c4b4
@@ -0,0 +1,13 @@
+public class MVCProjet {
+
+	public static void main(String[] args) {
+		ProjetView theView = new ProjetView();
+		ProjetModel theModel = new ProjetModel();
+		ProjetController theController = new ProjetController(theView, theModel);
+		 
+		Object selectedItem = theView.choixPort.getSelectedItem();
+
+	        String com = selectedItem.toString();
+	        theController.SimpleRead(com);
+	}
+}
--- Java_Lulu/src/MVCProjet.java
+++ Java_Lulu/src/MVCProjet.java
@@ -3,11 +3,7 @@ public class MVCProjet {
 	public static void main(String[] args) {
 		ProjetView theView = new ProjetView();
 		ProjetModel theModel = new ProjetModel();
-		ProjetController theController = new ProjetController(theView, theModel);
-		 
-		Object selectedItem = theView.choixPort.getSelectedItem();
+		ProjetController theController = new ProjetController(theView, theModel, theModel.monCommPort);
 
-	        String com = selectedItem.toString();
-	        theController.SimpleRead(com);
 	}
 }
